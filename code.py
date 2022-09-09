704b66b809f7cdb2934d1c50008e9a0520aa6735
vjones2906 <vjones2906@charlottesvilleschools.org>
2022-09-09 11:36:15 -0400

    red green blue neopixel blink version 1

--- code.py
+++ code.py
@@ -1,5 +1,22 @@
 from time import sleep
+import board
+import neopixel
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.5 
+
+print("Make it red!")
 
 while True:
-    print("Hello world!")
+    dot.fill((0, 0, 255))
+    sleep(1)
+    dot.fill((0, 0, 0))
+    sleep(1)
+    dot.fill((255, 0, 0))
+    sleep(1)
+    dot.fill((0, 0, 0))
+    sleep(1)
+    dot.fill((0, 255, 0))
+    sleep(1)
+    dot.fill((0, 0, 0))
     sleep(1)
