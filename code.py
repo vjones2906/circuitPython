f4fd94b269a6f8426c02d473c63449052b5fbd21
vjones2906 <vjones2906@charlottesvilleschools.org>
2022-09-12 11:23:31 -0400

    flashes rainbow, includes loop counter

--- code.py
+++ code.py
@@ -2,21 +2,26 @@ from time import sleep
 import board
 import neopixel
 
-dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.5 
 
-print("Make it red!")
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.1 
+
+count=0
 
 while True:
-    dot.fill((0, 0, 255))
-    sleep(1)
-    dot.fill((0, 0, 0))
-    sleep(1)
+    count = count + 1
     dot.fill((255, 0, 0))
-    sleep(1)
-    dot.fill((0, 0, 0))
-    sleep(1)
+    sleep(.15)
+    dot.fill((255, 165, 0   ))
+    sleep(.15)
+    dot.fill((255, 255, 0))
+    sleep(.15)
     dot.fill((0, 255, 0))
-    sleep(1)
-    dot.fill((0, 0, 0))
-    sleep(1)
+    sleep(.15)
+    dot.fill((0, 0, 255))
+    sleep(.15)
+    dot.fill((75, 0, 130))
+    sleep(.15)
+    dot.fill((238, 130, 238))
+    sleep(.15)
+    print('done with loop', count)
