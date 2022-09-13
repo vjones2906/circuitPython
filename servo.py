c18fba4d4145164b428a8b59c4ca3d41fd5e55e8
vjones2906 <vjones2906@charlottesvilleschools.org>
2022-09-13 12:12:41 -0400

    finished code with buttons working

--- servo.py
+++ servo.py
@@ -1,13 +1,36 @@
-"""CircuitPython Essentials Capacitive Touch example"""
+"""CircuitPython Essentials Servo standard servo example"""
 import time
 import board
-import touchio
+import pwmio
+from adafruit_motor import servo
 
-touch_pad = board.A0
+from digitalio import DigitalInOut, Direction, Pull
 
-touch = touchio.TouchIn(touch_pad)
+button_a = DigitalInOut(board.D1)
+button_a.direction = Direction.INPUT
+button_a.pull = Pull.UP
+
+button_b = DigitalInOut(board.D2)
+button_b.direction = Direction.INPUT
+button_b.pull = Pull.UP
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
 
 while True:
-    if touch.value:
-        print("Touched!")
-    time.sleep(0.05)
+    if button_a.value:
+        for angle in range(0, 180, 10):  # 0 - 180 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            print("servo counterclockwise")
+            time.sleep(0.05)
+    elif button_b.value:
+        for angle in range(180, 0, -10): # 180 - 0 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            print("servo clockwise")
+            time.sleep(0.05)
+    else:
+        print("servo off")
+        time.sleep(0.5)
