f0c95fd2cf920469b939c8bb7e42c04f9bf1fdbb
vjones2906 <vjones2906@charlottesvilleschools.org>
2022-09-13 10:57:34 -0400

    renamed

---
+++ servo.py
@@ -0,0 +1,13 @@
+"""CircuitPython Essentials Capacitive Touch example"""
+import time
+import board
+import touchio
+
+touch_pad = board.A0
+
+touch = touchio.TouchIn(touch_pad)
+
+while True:
+    if touch.value:
+        print("Touched!")
+    time.sleep(0.05)
