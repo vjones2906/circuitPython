7af96ba0abaf2248866d77980046226cd1391fd5
vjones2906 <vjones2906@charlottesvilleschools.org>
2022-09-19 11:10:25 -0400

    finished product

--- ultrasonic.py
+++ ultrasonic.py
@@ -2,33 +2,38 @@ import time
 import board
 import neopixel
 import adafruit_hcsr04
-import simpleio
+import simpleio                     #imported lib
+
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.1 
-# neopixel turn red when your object is less than 5cm, blue when between 5 and 20cm, and green when farther than 20cm
+dot.brightness = 0.1                #setting up ultrasonic and neopixel
 
 while True:
     try:
-        cm=int(sonar.distance)
+        cm=int(sonar.distance)      #defining cm as a variable of what the ultrasonic sensor is reading
         print(cm)
         if cm<5: 
-            dot.fill((255, 0, 0))
+            dot.fill((255, 0, 0))   #if the distance is less than 5 cm, red
             time.sleep(.15)
         elif 5<=cm<20:
-        elif 20<=cm<=35:
-            
             red=simpleio.map_range(sonar.distance,5,20,255,0)
-            blue=simpleio.map_range(sonar.distance,5,20,255,0)
-            dot.fill((red, green, blue))
-            time.sleep(.15)
+            green=simpleio.map_range(sonar.distance,5,20,0,0)
+            blue=simpleio.map_range(sonar.distance,5,20,0,255)
+            dot.fill((red,green,blue))                   #smoothly trasition from red to blue as the distance goes from 5 to 20
+            time.sleep(.05)
+        elif 20<=cm<=35:   
+            red=simpleio.map_range(sonar.distance,20,35,0,0)
+            green=simpleio.map_range(sonar.distance,20,35,0,255)
+            blue=simpleio.map_range(sonar.distance,20,35,255,0)
+            dot.fill((red, green, blue))                 #smoothly trasition from blue to green as the distance goes from 20 to 35
+            time.sleep(.05)
         elif cm>35:
-            dot.fill((0, 255, 0))
+            dot.fill((0, 255, 0))                        #if the distance is more than 35 cm, green
             time.sleep(.15)
         else:
-            dot.fill((0, 0, 0))
+            dot.fill((0, 0, 0))                          #if there is no reading, off
             time.sleep(.15)
-    except RuntimeError:
+    except RuntimeError:                                 #if there is no value found, tell us
         print("Retrying!")
-    time.sleep(0.1)
+    time.sleep(0.1)                                      #debounce
     
