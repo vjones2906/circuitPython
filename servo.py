479ea2bc47e631d4b85c760a9a7c0973319a6261
vjones2906 <vjones2906@charlottesvilleschools.org>
2023-05-26 11:42:45 -0400

    servo done

--- servo.py
+++ servo.py
@@ -1,36 +1,28 @@
-"""CircuitPython Essentials Servo standard servo example"""
-import time
-import board
+#thanks to afton for code guidelines 
+import board                                                           #importing libraries
+from time import sleep
 import pwmio
 from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction
+angle = 90
 
-from digitalio import DigitalInOut, Direction, Pull
 
-button_a = DigitalInOut(board.D1)
-button_a.direction = Direction.INPUT
-button_a.pull = Pull.UP
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)         #connecting to servo
 
-button_b = DigitalInOut(board.D2)
-button_b.direction = Direction.INPUT
-button_b.pull = Pull.UP
+                                                                
+my_servo = servo.Servo(pwm)                                            #Create a servo object
+ 
+button = DigitalInOut(board.D1)                                        #telling what pins to input from the buttons
+button.direction = Direction.INPUT
+button2 = DigitalInOut(board.D2)
+button2.direction = Direction.INPUT
 
-# create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
-
-# Create a servo object, my_servo.
-my_servo = servo.Servo(pwm)
 
 while True:
-    if button_a.value:
-        for angle in range(0, 180, 10):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
-            print("servo counterclockwise")
-            time.sleep(0.05)
-    elif button_b.value:
-        for angle in range(180, 0, -10): # 180 - 0 degrees, 5 degrees at a time.
-            my_servo.angle = angle
-            print("servo clockwise")
-            time.sleep(0.05)
-    else:
-        print("servo off")
-        time.sleep(0.5)
+    if button.value and angle < 180:                                  #telling the servo to move by 5 if the button is pressed
+        angle += 5
+    if button2.value and angle > 0:
+        angle -=5
+    print(angle)
+    my_servo.angle = angle
+    sleep(0.01)  
